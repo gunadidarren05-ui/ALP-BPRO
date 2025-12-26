18bd8b41452288c2bf21c73b8c1f6d0fd7e8a8ba
Darren <gunadidarren05@gmail.com>
2025-12-26 21:09:13 +0700

    Implement role selection for the player
    Added user input for selecting starting roles in the game.

--- ALP_Darren Patr Brian Devon_BPRO/src/Main.java
+++ ALP_Darren Patr Brian Devon_BPRO/src/Main.java
@@ -1,8 +1,36 @@
-import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
-    private static final ArrayList <Team> characterList = new ArrayList<>();
     public static void main(String[] args) {
-        
+        Scanner input =  new Scanner(System.in);
+        System.out.println("Welcome to the League!");
+        System.out.println("Here, you're going to team up with other heroes to fight your way to the top.\n But first, time to choose your desired role!");
+        System.out.println("Select your starting roles (1-5): ");
+        System.out.println("1. Tank");
+        System.out.println("2. Mage");
+        System.out.println("3. Assassin");
+        System.out.println("4. Healer");
+        System.out.println("5. Fighter");
+        int choice = input.nextInt();
+        switch (choice) {
+            case 1:
+                //;
+                break;
+            case 2:
+                //;
+                break;
+            case 3:
+                //;
+                break;
+            case 4:
+                //;
+                break;
+            case 5:
+                //;
+                break;
+            default: System.out.println("Invalid choice. Please try again.");
+
+
+        }
     }
 }
